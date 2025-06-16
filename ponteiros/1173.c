d4b73785ca53951469630c4a3ffbb47898c5cde2
Orijnat <cledivanleao@gmail.com>
2025-06-16 13:29:23 -0300

    exercicios da pasta ponteiros concluidos

--- ponteiros/1173.c
+++ ponteiros/1173.c
@@ -1,3 +1,23 @@
-//
-// Created by TECHCELL on 30/05/2025.
-//
+#include <stdio.h>
+
+int main ()
+{
+
+    int n[10], i, x, k;
+
+    scanf("%d", &x);
+
+    for (k = 0, i = x; k < 10; i *= 2, k++)
+    {
+
+        n[k] = i;
+
+    }
+
+    for (i = 0; i < 10; i++)
+    {
+
+        printf("N[%d] = %d\n", i, n[i]);
+
+    }
+}
